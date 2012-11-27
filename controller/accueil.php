f993f43b4985d0956e557a4ef11982238e970ec5
Thomas DOS SANTOS <dos88@hotmail.fr>
2012-11-27 11:54:03 +0100

    test
    suprime la ligne test dans accueil.php

--- controller/accueil.php
+++ controller/accueil.php
@@ -10,5 +10,6 @@
 	<a href="index.php?page=auth">Se connecter </a> <br />
 	<a href="index.php?page=recherche">Rechercher </a> <br />
 	<a href="index.php?page=inscription">S'inscrire </a> <br />
+        <p>test</p>
 	</body>
 </html>
