39bc5e517fab4126a275f5c0adb7375c738660b4
Thomas DOS SANTOS <dos88@hotmail.fr>
2012-11-27 15:17:49 +0100

    CSS
    css

--- view/accueil.php
+++ view/accueil.php
@@ -1,15 +1,20 @@
 <!doctype html>
 <html>
 	<head>
-		<title>Insta Dating</title>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <link rel="stylesheet" href="/dating/src/css/style.css" />
+            <title>Insta Dating</title>
 	</head>
 	
 	<body>
-	<?php echo "ceci est la page accueil"; ?>
-	<br />
-	<a href="index.php?page=auth">Se connecter </a> <br />
-	<a href="index.php?page=recherche">Rechercher </a> <br />
-	<a href="index.php?page=inscription">S'inscrire </a> <br />
-        <p>test</p>
+         <h1>Insta Dating</h1>    
+         <img class="logo" src="/dating/src/img/tn_logo.jpg" alt="logo" />
+         <img class="couple" src="/dating/src/img/couple.jpg" alt="couple" /> 
+         
+	
+	<a class="connection" href="index.php?page=auth">Se connecter </a> <br />
+	<a href="/../index.php?page=recherche">Rechercher </a> <br />
+	<a href="/../index.php?page=inscription">S'inscrire </a> <br />
+        
 	</body>
 </html>
